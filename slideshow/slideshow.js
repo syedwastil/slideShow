36028117e65689cd129c2576f054e2ec73f457df
Muhammad Abdur Rehman Wasti <45505896+syedwastil@users.noreply.github.com>
2022-11-28 12:19:10 +0500

    Done

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,15 +1,88 @@
-
+var curr=1;
+var x=null;
 function slide(){
-    var curr=1
-    setInterval(()=>{
-        console.log(document.getElementById("imag").currentSrc)
+  
+    document.getElementById("ab").addEventListener("click",()=>{
+        console.log("ab check")
+        autoBackword()
+    })
+    document.getElementById("prev").addEventListener("click",()=>{
+        console.log("prev check")
+        prev()
+    })
+    document.getElementById("stop").addEventListener("click",()=>{
+        console.log("stp check")
+        stop()
+    })
+    document.getElementById("fwd").addEventListener("click",()=>{
+        console.log("fwd check")
+        fwd()
+    })
+    document.getElementById("af").addEventListener("click",()=>{
+        console.log("af check")
+        autoForward()
+    })
+}
+
+
+function autoBackword(){
+    
+    x=setInterval(()=>{
+        console.log("ab is running")
+        if(curr==1){
+            curr=3
+        }else{
+            curr-=1
+        }
         document.getElementById("imag").src=`https://cyf-image-carousel.netlify.app/sample-images/kitten${curr}.jpg`
+
+        
+    },2000)
+}
+function prev(){
+
+
+        if(curr==1){
+            curr=3
+        }else{
+            curr-=1
+        }
+        document.getElementById("imag").src=`https://cyf-image-carousel.netlify.app/sample-images/kitten${curr}.jpg`
+   
+        
+    }
+function fwd(){
+
+
         if(curr==3){
             curr=1
         }else{
             curr+=1
         }
+        document.getElementById("imag").src=`https://cyf-image-carousel.netlify.app/sample-images/kitten${curr}.jpg`
+
+    }
+function stop(){
+        clearInterval(x)
+        document.getElementById("imag").src=`https://cyf-image-carousel.netlify.app/sample-images/kitten${curr}.jpg`
+
+        
+    }
+
+function autoForward(){
+
+    console.log("autof ",curr)
+    x=setInterval(()=>{
+        console.log("af is running")
+
+        if(curr==3){
+            curr=1
+        }else{
+            curr+=1
+        }
+        document.getElementById("imag").src=`https://cyf-image-carousel.netlify.app/sample-images/kitten${curr}.jpg`
         
     },2000)
 }
-slide()
+
+window.onload=slide
